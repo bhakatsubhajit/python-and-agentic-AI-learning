637c713f08e499348c87e3b500dc8b47084ade2f
SubhajitBhakat <bhakatsubhajit13@gmail.com>
2026-03-31 22:52:50 +0530

    default arguement example added

--- Python Basics and Sample Codes/functions/functionArguments.py
+++ Python Basics and Sample Codes/functions/functionArguments.py
@@ -6,3 +6,14 @@ def add(a,b):
 c = add(8,7)
 
 print("The sum is == ",c)
+
+#default arguements
+
+def multi(a,b=1):
+    return a*b
+
+result = multi(5)
+updatedResult = multi(5,2)
+
+print("result with default parameter = ", result)
+print("result = ", updatedResult)
