f70744394028ec5df68255dcfd4816263a1710b2
SubhajitBhakat <bhakatsubhajit13@gmail.com>
2026-04-11 20:04:23 +0530

    list example

---
+++ Python Basics and Sample Codes/List/listEx.py
@@ -0,0 +1,10 @@
+# lists are ordered, mutable, and allow duplicate elements.
+marks = [80,70, 45,65,20]
+mixed = [10.09, 45, "Hello", True]
+
+print(marks[-1])
+print(marks[3])
+print(marks[2:4])
+
+marks.append(85)
+print(marks)
