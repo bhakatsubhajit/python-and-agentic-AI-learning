3775f39c8894bcf3ae461e4dc22cf5a4eac8e770
SubhajitBhakat <bhakatsubhajit13@gmail.com>
2026-04-12 00:15:49 +0530

    tuples example

---
+++ Python Basics and Sample Codes/Tuples/tupleUnpacking.py
@@ -0,0 +1,8 @@
+newTuples = (30,92,57,80,92)
+
+a, b ,c, d, e = newTuples
+
+print(a,b,c)
+print(newTuples.count(92)) # counts the number of occurrences of 92 in the tuple.
+
+print(newTuples.index(92)) # finds the index of the first occurrence of 92 in the tuple.
---
+++ Python Basics and Sample Codes/Tuples/tuplesEx.py
@@ -0,0 +1,10 @@
+newTuples = (30,67,12,22)
+
+print(newTuples[2])
+
+#newTuples[2] = 31 # Tuples are immuatable 
+
+'''We use tuples when we don't want the values to be changed'''
+
+singleTuple = (5,)
+print(type(singleTuple))
